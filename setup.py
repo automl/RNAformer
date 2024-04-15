850ba3388fd91dbf361aeaf3b388828c032a255f
ivelet <ryankoeksal@gmail.com>
2024-04-16 00:16:57 +0200

    Bug fixes and PyPI package files

--- setup.py
+++ setup.py
@@ -3,8 +3,12 @@
 import shutil
 from distutils.core import setup
 
+from setuptools import find_packages
+
 
 setup(name='RNAformer',
-      version='latest',
+      version='1.0.0',
+      packages=find_packages(where='RNAformer'),
+      package_dir={'': 'RNAformer'},
       dependency_links=[],
       )
