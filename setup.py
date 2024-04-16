067a99a08717c2d73f94d75859c74edf1b830d87
ivelet <ryankoeksal@gmail.com>
2024-04-16 14:53:13 +0200

    Added new PyPI release files with removed author emails

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages
 
 
 setup(name='RNAformer',
-      version='1.0.0',
+      version='1.0.1',
       packages=find_packages(where='RNAformer'),
       package_dir={'': 'RNAformer'},
       dependency_links=[],
